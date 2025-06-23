b9f103b6e72197bb4a60b2ec54f43ff488d1a3a1
forknay <julienyang12@gmail.com>
2025-06-23 11:04:57 -0700

    create dict to count nb of bytes to each gpu (technically a host operation)

--- perf_model.py
+++ perf_model.py
@@ -1,7 +1,6 @@
 from simulation import *
 
-WEIGHT_PRECISION = 32
-ROUTING_PRECISION = 8
-def convert_to_bytes():
-    # i will do this later
-    pass
+WEIGHT_PRECISION = 4 # FP32 bytes
+ROUTING_PRECISION = 1 # INT8 bytes
+def convert_to_bytes(weights, routing):
+    d = {i: 0 for i in range(NB_NODES)}
