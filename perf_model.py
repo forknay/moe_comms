4d6700de489928147d2894a1b80828b31e355819
forknay <julienyang12@gmail.com>
2025-06-24 15:50:13 -0700

    added assumptions and changes all nb to num for consistency

--- perf_model.py
+++ perf_model.py
@@ -1,14 +1,29 @@
 from simulation import *
 
+## ASSUMING FULL MESH TOPOLOGY 
+
+# Assumptions
+# No loss
+# No overhead
+# No memory allocation constraints, could just reserve space for decoding, not sure for prefill
+
+NUM_LAYERS = 61
+# Data conversion
 WEIGHT_PRECISION = 4 # FP32 bytes
 ROUTING_PRECISION = 1 # INT8 bytes
 ID_PRECISION = 2 # INT16 bytes
-
-NB_EXPERTS_PER_NODE = NUM_EXPERTS // NB_NODES
+NUM_EXPERTS_PER_NODE = NUM_EXPERTS // NUM_NODES
 UNIT_COMM_LOAD = (WEIGHT_PRECISION + 2*ID_PRECISION + ROUTING_PRECISION)
 
+# Infrastructure 
+NUM_LINKS = 1 # Number of links between two nodes
+HOST = True
+BASE_DELAY = 2 # in ms
+INTRA_BW = 100 # in GB/s
+INTER_BW = 50 # in GB/s
+
 def convert_to_bytes(weights, routing):
-    d = {i: 0 for i in range(NB_NODES)}
+    d = {i: 0 for i in range(NUM_NODES)}
     nb_recurrent = 0
     for i, route in enumerate(routing):
         route = route.tolist()
@@ -17,7 +32,7 @@ def convert_to_bytes(weights, routing):
             route.remove(int(weights[i][-2]))
             nb_recurrent += 1
         for expert in route:
-            d[expert//NB_EXPERTS_PER_NODE] += UNIT_COMM_LOAD # Expert weight and ID and chosen expert (irrelevant in the case when there's only one expert per node)
+            d[expert//NUM_EXPERTS_PER_NODE] += UNIT_COMM_LOAD # Expert weight and ID and chosen expert (irrelevant in the case when there's only one expert per node)
     return d, nb_recurrent
             
 
--- simulation.py
+++ simulation.py
@@ -8,18 +8,13 @@ EMBED_DIM = 7168             # Embedding dimension size
 
 HOT_RATIO = 0.5              # Ratio of hot experts 
 HOT_WEIGHT = 0.8             # Weight for hot experts 
-NB_HOT_EXPERTS = int(NUM_EXPERTS * HOT_RATIO)  # Number of hot experts
+NUM_HOT_EXPERTS = int(NUM_EXPERTS * HOT_RATIO)  # Number of hot experts
 
-NB_NODES = 8
+NUM_NODES = 8
 
 class Gate:
     """Mixture-of-Experts routing gate simulation."""
-    def __init__(self):
-        self.num_experts = NUM_EXPERTS
-        self.seqlen = SEQLEN
-        self.top_k = TOP_K
-        self.embed_dim = EMBED_DIM
-        self.nb_nodes = NB_NODES
+
     @staticmethod
     def generate_routing() -> tuple[np.ndarray, np.ndarray]:
         """
@@ -29,16 +24,16 @@ class Gate:
             mock_weights (np.ndarray): Random weights for each token-expert pair.
             routing_table (np.ndarray): Routing table of expert indices per token.
         """
-        hot_experts = random.sample(range(NUM_EXPERTS), NB_HOT_EXPERTS)
+        hot_experts = random.sample(range(NUM_EXPERTS), NUM_HOT_EXPERTS)
         #print(hot_experts)
         cold_experts = list(set(range(NUM_EXPERTS)) - set(hot_experts))
-        if NB_HOT_EXPERTS == NUM_EXPERTS:
+        if NUM_HOT_EXPERTS == NUM_EXPERTS:
             expert_weights = [1 / NUM_EXPERTS] * NUM_EXPERTS
         else:
             # Assuming we want a balanced distribution within groups
             expert_weights = ( 
-                [HOT_WEIGHT / NB_HOT_EXPERTS] * NB_HOT_EXPERTS +
-                [(1 - HOT_WEIGHT) / (NUM_EXPERTS - NB_HOT_EXPERTS)] * (NUM_EXPERTS - NB_HOT_EXPERTS)
+                [HOT_WEIGHT / NUM_HOT_EXPERTS] * NUM_HOT_EXPERTS +
+                [(1 - HOT_WEIGHT) / (NUM_EXPERTS - NUM_HOT_EXPERTS)] * (NUM_EXPERTS - NUM_HOT_EXPERTS)
             )
 
         all_experts = hot_experts + cold_experts
@@ -57,7 +52,7 @@ class Gate:
             routing (tuple[np.ndarray, np.ndarray]): Updated token weights with NPU indices for combine and token indices for reordering (batch can be determined by token index).
         """
         mock_weights, routing_table = routing
-        mock_weights = [(*mock_weights[i], np.int16(i % NB_NODES), np.int16(i)) for i in range(SEQLEN)]
+        mock_weights = [(*mock_weights[i], np.int16(i % NUM_NODES), np.int16(i)) for i in range(SEQLEN)]
         return mock_weights, routing_table
 
 def export_routing(routing: tuple[np.ndarray, np.ndarray]) -> None:
@@ -94,7 +89,7 @@ if __name__ == "__main__":
     print("Samples: ", gate_output[0][0], gate_output[1][0])
     labelled_output = Gate.npu_identify(gate_output)
     print("Labelled output (*weights, NPU, token): ", labelled_output[0][0], labelled_output[1][0])
-    true_hot_experts = np.argpartition(np.unique(labelled_output[1], return_counts=True)[1], -NB_HOT_EXPERTS)[-NB_HOT_EXPERTS:]
+    true_hot_experts = np.argpartition(np.unique(labelled_output[1], return_counts=True)[1], -NUM_HOT_EXPERTS)[-NUM_HOT_EXPERTS:]
     #print("Check hot_experts: ", true_hot_experts)
     print("Hot experts load: ", sum(np.unique(labelled_output[1], return_counts=True)[1][true_hot_experts])/(SEQLEN*TOP_K))
     export_routing(labelled_output)
